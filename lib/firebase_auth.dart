e318b21a990084b44873af02fffd4e3166ebdf66
bwaobikeze <bwaobikeze@hotmail.com>
2024-02-11 02:34:46 -0600

    Implemented navigation between screens when signing in/out & connected firebase auth to app

---
+++ lib/firebase_auth.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FieAuth {
+  Future<User?> signIn(String email, String password) async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  Future<User?> signUp(String email, String password) async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+  signout () async {
+    await FirebaseAuth.instance.signOut();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_auth.dart';
 
 //Main function to run the app
-Future<void> main() async{
-    WidgetsFlutterBinding.ensureInitialized();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const AnimeWatchlistApp());
-  
-
 }
+
 //Main class for the app
 class AnimeWatchlistApp extends StatelessWidget {
   const AnimeWatchlistApp({Key? key}) : super(key: key);
@@ -24,126 +24,184 @@ class AnimeWatchlistApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:LoginScreen(),
+      home: LoginScreen(),
     );
   }
-
 }
-//Login screen
-  class LoginScreen extends StatelessWidget {
-    var username = '';
-    var password = '';
-  
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Login'),
-        ),
-        body: const Center(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: LoginForm(),
-          ),
-        ),
-      );
-    }
-    
-  }
-  //Login form
-  class LoginForm extends StatelessWidget {
-    const LoginForm({Key? key}) : super(key: key);
-  
-    @override
-    Widget build(BuildContext context) {
-      return Column(
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Username',
-            ),
-            onChanged: (text) {
-              print('First text field: $text');
-            },
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Password',
-            ),
-            onChanged: (text) {
-              print('Second text field: $text');
-            },
-          ),
-          ElevatedButton(
-            onPressed: () {
-              print('Login button pressed');
-            },
-            child: const Text('Login'),
-          ),
-              ElevatedButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => createAccount()));
-            },
-            child: const Text('Create Account'),
-          ),
-        ],
-      );
-    }
-  }
-  //Create account screen
-  class createAccount extends StatelessWidget {
-    var username = '';
-    var password = '';
-  
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Create Account'),
-        ),
-        body: const Center(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: createAccountForm(),
-          ),
-        ),
-      );
-    }
-    
-  }
-  //Create account form
-  class createAccountForm extends StatelessWidget {
-    const createAccountForm({Key? key}) : super(key: key);
-  
-    @override
-    Widget build(BuildContext context) {
-      return Column(
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Username',
-            ),
-            onChanged: (text) {
-              print('First text field: $text');
-            },
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Password',
-            ),
-            onChanged: (text) {
-              print('Second text field: $text');
-            },
-          ),
-          ElevatedButton(
-            onPressed: () {
-              print('Create Account button pressed');
-            },
-            child: const Text('Create Account'),
-          ),
-        ],
-      );
-    }
-  }
-  
 
+//Login screen
+class LoginScreen extends StatelessWidget {
+  var username = '';
+  var password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: LoginForm(),
+        ),
+      ),
+    );
+  }
+}
+
+//Login form
+class LoginForm extends StatefulWidget {
+  LoginForm({Key? key}) : super(key: key);
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  User? user;
+  FieAuth fieAuth = FieAuth();
+  var username = '';
+  var password = '';
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'Username',
+          ),
+          onChanged: (text) {
+            print('First text field: $text');
+            username = text;
+          },
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'Password',
+          ),
+          onChanged: (text) {
+            print('Second text field: $text');
+            password = text;
+          },
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            print('Login button pressed');
+            user = await fieAuth.signIn(username, password);
+            if (user != null) {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => homeScreen()));
+            }
+          },
+          child: const Text('Login'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => createAccount()));
+          },
+          child: const Text('Create Account'),
+        ),
+      ],
+    );
+  }
+}
+
+//Create account screen
+class createAccount extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create Account'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: createAccountForm(),
+        ),
+      ),
+    );
+  }
+}
+
+//Create account form
+class createAccountForm extends StatefulWidget {
+  createAccountForm({Key? key}) : super(key: key);
+
+  @override
+  State<createAccountForm> createState() => _createAccountFormState();
+}
+
+class _createAccountFormState extends State<createAccountForm> {
+  User? user;
+  FieAuth fieAuth = FieAuth();
+  var username = '';
+
+  var password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'Username',
+          ),
+          onChanged: (text) {
+            print('First text field: $text');
+            username = text;
+          },
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'Password',
+          ),
+          onChanged: (text) {
+            print('Second text field: $text');
+            password = text;
+          },
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            print('Create Account button pressed');
+            user = await fieAuth.signUp(username, password);
+            if (user != null) {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => homeScreen()));
+            }
+          },
+          child: const Text('Create Account'),
+        ),
+        ElevatedButton(onPressed: () {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => LoginScreen()));
+        }, child: const Text('Back to Login')),
+      ],
+    );
+  }
+}
+//Home screen
+class homeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        leading: ElevatedButton(
+          onPressed: () {
+            FieAuth fieAuth = FieAuth();
+            fieAuth.signout();
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => LoginScreen()));
+          },
+          child: const Text('Logout'),
+        ),
+      ),
+      body: Center(
+        child: const Text('Welcome to the Anime Watchlist App'),
+      ),
+    );
+  }
+}
