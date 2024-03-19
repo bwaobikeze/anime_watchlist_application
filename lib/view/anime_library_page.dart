4113ce8fab2f53094f23dd672e28d5543499cdc0
bwaobikeze <bwaobikeze@hotmail.com>
2024-03-19 05:40:41 -0500

    added UI to the anime Info page

--- lib/view/anime_library_page.dart
+++ lib/view/anime_library_page.dart
@@ -1,14 +1,21 @@
-import 'package:flutter/material.dart';
-class animeLibraryPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Anime Library'),
-      ),
-      body: Center(
-        child: Text('Anime Library'),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:graphql_flutter/graphql_flutter.dart';
+
+// class animeLibraryPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Anime Library'),
+//       ),
+//       body: Query(
+//           options:
+//               QueryOptions(document: gql(), variables: {'id': variable}),
+//           builder: (QueryResult result,
+//               {Refetch? refetch, FetchMore? fetchMore}) {
+
+
+//               }),
+//     );
+//   }
+// }
